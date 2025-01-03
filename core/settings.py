e61827e4da9aeb04892132ec7383df2d3f7a6d42
GabrielSilvaCoder <g.silva01@icloud.com>
2025-01-02 21:58:35 -0300

    Fix rabbitmq credentials

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'pyamqp://admin:admin@localhost//'
+CELERY_BROKER_URL = 'pyamqp://guest:guest@localhost//'
 CELERY_TIMEZONE = 'America/Sao_Paulo'
 CELERY_RESULT_BACKEND = 'django-db'
